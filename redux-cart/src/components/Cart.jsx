701ec7bdb39f96f4637ebfceeff1d4e9581bced2
srikanth4c <srikanthbinf@gmail.com>
2020-07-13 21:18:51 +0530

    react-cart-III update cart component

--- redux-cart/src/components/Cart.jsx
+++ redux-cart/src/components/Cart.jsx
@@ -2,74 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
 import {addToOrders, increment, decrement} from "../redux/action";
+// import './fontAwesome/fontAwesome';
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Cart extends Component {
     constructor(props) {
         super(props);
         this.state={
             name:'',
+            mobile:'',
             address:''
         }
     }
-
-    handleChange=(e)=>{
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
-    render() {
-        const {data,addToOrders,increment, decrement}=this.props
-        return (
-            <div className="container">
-                <div>
-                    <label>Name: </label>
-                    <input type="text" name="name" onChange={this.handleChange} />
-                    <label>Address: </label>
-                    <input type="text" name="address" onChange={this.handleChange} />
-                    <Link to='/order'>
-                        <button onClick={()=>addToOrders({name:this.state.name, 
-                        address:this.state.address,
-                        data:data})} className="btn btn-outline-warning">Add To Orders</button>
-                    </Link>
-                </div>
-                {
-                    data && data.map((ele,index)=>{
-                        return(
-                            // <SingleItem key={index} data={ele} />
-                            <div>
-                                <table  key={index}className="table table-striped table-dark">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">Id</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Price</th>
-                                        <th scope="col">Category</th>
-                                        <th scope="col">Image</th>
-                                        <th scope="col">Qty</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr key={index}>
-                                        <th scope="row" >{ele.id}</th>
-                                        <td>{ele.name}</td>
-                                        <td>{ele.price}</td>
-                                        <td>{ele.category}</td>
-                                        <td><img src={ele.img} alt={ele.name} width="100" height="100" /></td>
-                                        <td>
-                                            <button onClick={()=>increment(ele.id)} >+</button><br/>
-                                            {ele.qty}<br/>
-                                            <button onClick={()=>decrement(ele.id)}>-</button>
-                                        </td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-        </div>
-                        )
-                    })
-                }
+    render(){
+        return(
+            <div className='container'>
+                {/* Display cart items */}
+                        
             </div>
         )
     }
+  
 }
 
 
--- redux-cart/src/components/SingleItem.jsx
+++ redux-cart/src/components/SingleItem.jsx
@@ -8,67 +8,72 @@ export default class SingleItem extends React.Component {
             isAuth:false
         }
     }
-    render(){
-        const {data, isAuth, addToCart}=this.props
-        this.toggleAuth=()=>{
-            alert('user not logged In');
-            if(!isAuth){
+    componentDidUpdate(){
+        console.log('local auth')
+        console.log(this.state.isAuth)
+    }
+    toggleAuth=()=>{
+        alert('user not logged In');
+        if(!this.props.isAuth){
                 this.setState(
                     {
                         isAuth:!this.state.isAuth
                     }
                 )
-                alert('yes he is not logged in')
-                return <Redirect to='/login' />
             }
-        }
-    return (
-        <div>
-             <table className="table table-striped table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Image</th>
-                    {addToCart?'': <th scope="col">Qty</th>}
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row" ><Link to={`/products/${data.id}`}>{data.id}</Link></th>
-                    <td><Link to={`/products/${data.id}`}>{data.name}</Link></td>
-                    <td>{data.price}</td>
-                    <td>{data.category}</td>
-                    <td><Link to={`/products/${data.id}`}>
-                            <img src={data.img} alt={data.name} width="100" height="100" />
-                        </Link>
-                    </td>
-                    {
-                        addToCart ?
-                                    <td>
-                                        <button 
-                                            onClick={
-                                                ()=> {
-                                                    return(
-                                                        this.state.isAuth?
-                                                        addToCart(data.id):this.toggleAuth()
-                                                    )
-                                                }
-                                            } 
-                                            className="btn btn-outline-secondary"
-                                            >
-                                                Add to Cart
-                                            </button>
-                                            </td>
-                        :<td>{data.qty}</td>
-                    }
-                    {/* <td>{data.qty}</td> */}
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    )
-}
+        } 
+    render(){
+        const {data, isAuth, addToCart}=this.props
+        if(this.state.isAuth){
+            return <Redirect to='/login'/>
+        }    
+        return (
+            <div>
+                <table className="table table-striped table-dark">
+                    <thead>
+                        <tr>
+                            <th scope="col">Id</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Image</th>
+                            {addToCart?'': <th scope="col">Qty</th>}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row" ><Link to={`/products/${data.id}`}>{data.id}</Link></th>
+                            <td><Link to={`/products/${data.id}`}>{data.name}</Link></td>
+                            <td>{data.price}</td>
+                            <td>{data.category}</td>
+                            <td><Link to={`/products/${data.id}`}>
+                                    <img src={data.img} alt={data.name} width="100" height="100" />
+                                </Link>
+                            </td>
+                            {
+                                addToCart ?
+                                            <td>
+                                                <button 
+                                                    onClick={
+                                                        ()=> {
+                                                            return(
+                                                                this.state.isAuth?
+                                                                addToCart(data.id):this.toggleAuth()
+                                                            )
+                                                        }
+                                                    }    
+                                                    className="btn btn-outline-secondary"
+                                                >
+                                                    Add to Cart
+                                                </button>
+                                                </td>
+                                :<td>{data.qty}</td>
+                            }
+                                {/* <td>{data.qty}</td> */}
+                            </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
